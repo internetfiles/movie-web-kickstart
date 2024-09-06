0a10b81945a72e26b03b07e365f05b80e7f085d8
Jacksparrowpro <70373238+internetfiles@users.noreply.github.com>
2024-09-06 08:41:38 +0300

    Update site-header.tsx

--- src/components/main/site-header.tsx
+++ src/components/main/site-header.tsx
@@ -1,10 +1,20 @@
 import { siteConfig } from "@/configs/site";
-import React from "react";
+import React, { useEffect } from "react";
 import MainNav from "@/components/navigation/main-nav";
 
 const SiteHeader = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = '//bracemascara.com/67/ae/e3/67aee39d51ff1a813d163c60a33e8c9d.js';
+    script.type = 'text/javascript';
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
-    // <header className="sticky top-0 z-50 border-b bg-background">
     <header className="sticky top-0 z-50">
       <MainNav items={siteConfig.mainNav} />
       {/* <MobileNav items={siteConfig.mainNav} className="md:hidden" /> */}
